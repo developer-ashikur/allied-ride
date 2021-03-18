02dbcd59655660f902e906989de9f0ae274016a4
Md Ashikur Rahman <developerashikur01@gmail.com>
2021-03-18 23:17:06 +0600

    project started

---
+++ src/firebase.config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyANfohkoa96JB65kZ74MQFK8Z6ZUDQOAtQ",
+    authDomain: "allied-ride.firebaseapp.com",
+    projectId: "allied-ride",
+    storageBucket: "allied-ride.appspot.com",
+    messagingSenderId: "900947679365",
+    appId: "1:900947679365:web:79f283a304ec9d7e6c31ec"
+  };
